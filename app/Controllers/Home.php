07a34b80f8327280d22e6bb35fc4308b6666d536
Mfrhn2405 <mfarhan14mei2019@gmail.com>
2025-11-21 18:07:11 +0700

    Percobaan Kedua Conflict

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,6 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('Selamat Datang, Muhammad Farhan');
+        return view('Selamat Datang, Prodi Manajemen Informatika');
     }
 }
